35629aa5ec2ea368d8d0def0e0a4bb23f27c655b
Adam Irby <airby@csumb.edu>
2018-10-21 18:19:49 +0000

    Adding link to lab 1 to home page.

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
 // then click the URL that is emitted to the Output tab of the console
 
 echo "Adam Irby's CST336 Home Page";
-
 ?>
+
+<h1>Labs</h1>
+<a href="http://airby-cst336.herokuapp.com/labs/lab1/">Lab 1</a>
 </body>
 </html>
