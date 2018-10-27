677d07a7e87419cefe1617eeae879cf0d3f12ab2
Adam Irby <airby@csumb.edu>
2018-10-27 06:19:20 +0000

    adding lab 2 and adding link to home page for lab 2

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <title>Adam Irby's CST336 Homepage</title>
         <meta charset="utf-8" />
     </head>
     
     <body>
+        <h1>Adam Irby's CST336 Homepage</h1>
         <div id="labs">
             <h1>Labs</h1>
             <ul>
                 <li><a href="labs/lab1">Lab 1</a></li>
+                <li><a href="labs/lab2">Lab 2</a></li>
             </ul>
         </div>
         
